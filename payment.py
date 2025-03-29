5ec6a63f40ec0c988aeac1ecfc1e886188309989
Your Name <you@example.com>
2025-03-29 13:22:35 +0300

    Modified the payment file to address bugs

--- payment.py
+++ payment.py
@@ -8,28 +8,28 @@ class Payment:
         self._status = "Pending"
         self._transaction_id = None
     
-    @property
-    def amount(self):
+    def get_amount(self):
         return self._amount
     
-    @property
-    def payment_date(self):
+    def get_payment_date(self):
         return self._payment_date
     
-    @property
-    def status(self):
+    def get_status(self):
         return self._status
     
-    @property
-    def transaction_id(self):
+    def get_transaction_id(self):
         return self._transaction_id
     
-    @status.setter
-    def status(self, new_status):
-        self._status = new_status
+    def set_amount(self, amount):
+        self._amount = amount
+    
+    def set_payment_date(self, payment_date):
+        self._payment_date = payment_date
+    
+    def set_status(self, status):
+        self._status = status
     
     def process_payment(self):
-        """Process the payment (to be implemented by subclasses)"""
         raise NotImplementedError
     
     def __str__(self):
@@ -37,33 +37,39 @@ class Payment:
                 f"Date: {self._payment_date}\n"
                 f"Status: {self._status}")
 
-
 class CreditCardPayment(Payment):
     """Credit card payment method"""
     
     def __init__(self, amount, payment_date, card_number, card_holder, expiry_date, cvv):
-        """Initialize a credit card payment"""
         super().__init__(amount, payment_date)
         self._card_number = card_number
         self._card_holder = card_holder
         self._expiry_date = expiry_date
         self._cvv = cvv
     
-    @property
-    def card_number(self):
+    def get_card_number(self):
         return self._card_number
     
-    @property
-    def card_holder(self):
+    def get_card_holder(self):
         return self._card_holder
     
-    @property
-    def expiry_date(self):
+    def get_expiry_date(self):
         return self._expiry_date
     
-    @property
-    def cvv(self):
+    def get_cvv(self):
         return self._cvv
+    
+    def set_card_number(self, card_number):
+        self._card_number = card_number
+    
+    def set_card_holder(self, card_holder):
+        self._card_holder = card_holder
+    
+    def set_expiry_date(self, expiry_date):
+        self._expiry_date = expiry_date
+    
+    def set_cvv(self, cvv):
+        self._cvv = cvv
     
     def process_payment(self):
         """Process the credit card payment"""
@@ -77,33 +83,39 @@ class CreditCardPayment(Payment):
                 f"Card Holder: {self._card_holder}\n"
                 f"Last 4 Digits: **** **** **** {self._card_number[-4:]}")
 
-
 class DebitCardPayment(Payment):
     """Debit card payment method"""
     
     def __init__(self, amount, payment_date, card_number, card_holder, expiry_date, cvv):
-        """Initialize a debit card payment"""
         super().__init__(amount, payment_date)
         self._card_number = card_number
         self._card_holder = card_holder
         self._expiry_date = expiry_date
         self._cvv = cvv
     
-    @property
-    def card_number(self):
+    def get_card_number(self):
         return self._card_number
     
-    @property
-    def card_holder(self):
+    def get_card_holder(self):
         return self._card_holder
     
-    @property
-    def expiry_date(self):
+    def get_expiry_date(self):
         return self._expiry_date
     
-    @property
-    def cvv(self):
+    def get_cvv(self):
         return self._cvv
+    
+    def set_card_number(self, card_number):
+        self._card_number = card_number
+    
+    def set_card_holder(self, card_holder):
+        self._card_holder = card_holder
+    
+    def set_expiry_date(self, expiry_date):
+        self._expiry_date = expiry_date
+    
+    def set_cvv(self, cvv):
+        self._cvv = cvv
     
     def process_payment(self):
         """Process the debit card payment"""
@@ -117,28 +129,28 @@ class DebitCardPayment(Payment):
                 f"Card Holder: {self._card_holder}\n"
                 f"Last 4 Digits: **** **** **** {self._card_number[-4:]}")
 
-
 class MobileWalletPayment(Payment):
     """Mobile wallet payment method"""
     
     def __init__(self, amount, payment_date, wallet_type, phone_number):
-        """Initialize a mobile wallet payment"""
         super().__init__(amount, payment_date)
         self._wallet_type = wallet_type
         self._phone_number = phone_number
     
-    @property
-    def wallet_type(self):
+    def get_wallet_type(self):
         return self._wallet_type
     
-    @property
-    def phone_number(self):
+    def get_phone_number(self):
         return self._phone_number
+    
+    def set_wallet_type(self, wallet_type):
+        self._wallet_type = wallet_type
+    
+    def set_phone_number(self, phone_number):
+        self._phone_number = phone_number
     
     def process_payment(self):
         """Process the mobile wallet payment"""
-        # In a real system, this would connect to a payment gateway
-        # For this example, we'll simulate a successful payment
         self._transaction_id = f"MW-{self._payment_date.strftime('%Y%m%d%H%M%S')}"
         self._status = "Completed"
         return True
