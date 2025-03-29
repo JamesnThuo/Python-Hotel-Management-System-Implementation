50773161ffa64a0c333cf908eb5f6792d1919cd7
Your Name <you@example.com>
2025-03-29 13:23:08 +0300

    Modified the room file to address bugs and correct child classes

--- room.py
+++ room.py
@@ -10,37 +10,42 @@ class Room:
         self._is_available = is_available
     
     # Getters
-    @property
     def room_number(self):
         return self._room_number
     
-    @property
-    def room_type(self):
+    def get_room_type(self):
         return self._room_type
     
-    @property
-    def amenities(self):
+    def get_amenities(self):
         return self._amenities
     
-    @property
-    def price_per_night(self):
+    def get_price_per_night(self):
         return self._price_per_night
     
-    @property
     def is_available(self):
         return self._is_available
     
-    # Setters
-    @is_available.setter
-    def is_available(self, availability):
-        self._is_available = availability
+    # setters
+    def set_room_number(self, room_number):
+        self._room_number = room_number
+    
+    def set_room_type(self, room_type):
+        self._room_type = room_type
+    
+    def set_amenities(self, amenities):
+        self._amenities = amenities
+    
+    def set_price_per_night(self, price_per_night):
+        self._price_per_night = price_per_night
+    
+    def set_available(self, is_available):
+        self._is_available = is_available
     
     def __str__(self):
         return (f"Room {self._room_number} - {self._room_type}\n"
                 f"Amenities: {', '.join(self._amenities)}\n"
                 f"Price per night: ${self._price_per_night:.2f}\n"
                 f"Available: {'Yes' if self._is_available else 'No'}")
-
 
 class StandardRoom(Room):
     """Standard room type with basic amenities"""
@@ -51,17 +56,20 @@ class StandardRoom(Room):
         self._max_occupancy = 2
         self._has_balcony = False
     
-    @property
-    def max_occupancy(self):
+    def get_max_occupancy(self):
         return self._max_occupancy
     
-    @property
     def has_balcony(self):
         return self._has_balcony
     
+    def set_max_occupancy(self, max_occupancy):
+        self._max_occupancy = max_occupancy
+    
+    def set_has_balcony(self, has_balcony):
+        self._has_balcony = has_balcony
+    
     def __str__(self):
         return super().__str__() + f"\nMax Occupancy: {self._max_occupancy}"
-
 
 class DeluxeRoom(Room):
     """Deluxe room type with additional amenities"""
@@ -72,17 +80,20 @@ class DeluxeRoom(Room):
         self._max_occupancy = 4
         self._has_balcony = True
     
-    @property
-    def max_occupancy(self):
+    def get_max_occupancy(self):
         return self._max_occupancy
     
-    @property
     def has_balcony(self):
         return self._has_balcony
     
+    def set_max_occupancy(self, max_occupancy):
+        self._max_occupancy = max_occupancy
+    
+    def set_has_balcony(self, has_balcony):
+        self._has_balcony = has_balcony
+    
     def __str__(self):
         return super().__str__() + f"\nMax Occupancy: {self._max_occupancy}\nBalcony: {'Yes' if self._has_balcony else 'No'}"
-
 
 class Suite(Room):
     """Suite room type with premium amenities"""
@@ -95,17 +106,23 @@ class Suite(Room):
         self._has_balcony = True
         self._separate_bedroom = True
     
-    @property
-    def max_occupancy(self):
+    def get_max_occupancy(self):
         return self._max_occupancy
     
-    @property
     def has_balcony(self):
         return self._has_balcony
     
-    @property
-    def separate_bedroom(self):
+    def has_separate_bedroom(self):
         return self._separate_bedroom
+    
+    def set_max_occupancy(self, max_occupancy):
+        self._max_occupancy = max_occupancy
+    
+    def set_has_balcony(self, has_balcony):
+        self._has_balcony = has_balcony
+    
+    def set_separate_bedroom(self, separate_bedroom):
+        self._separate_bedroom = separate_bedroom
     
     def __str__(self):
         return (super().__str__() + 
